289cfaf625220d17fc8657cfcf057be9c0271d81
rumisa94 <rumisaomar@hotmail.com>
2017-03-21 10:31:59 +0300

    firest unity project

---
+++ Assets/example.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class example : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hi developer");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
