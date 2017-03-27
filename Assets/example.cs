607d269904424d4f62ef9e53bafc217713505141
rumisa94 <rumisaomar@hotmail.com>
2017-03-27 08:57:16 +0300

    adding color properity
    make the color of the ball change

--- Assets/example.cs
+++ Assets/example.cs
@@ -2,16 +2,23 @@
 using System.Collections;
 
 public class example : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public Color start=Color.red;
+    public Color end= Color.green;
+    public Renderer ballR;
+    public float duration = 1.0F;
+    // Use this for initialization
+    void Start () {
         Debug.Log("Hi developer");
         Debug.Log("trying the reposotry");
-	
-	}
+        ballR = GetComponent<Renderer>();
+        
+}
 	
 	// Update is called once per frame
 	void Update () {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration; ;
+        ballR.material.color = Color.Lerp(start, end, lerp);
+
 	
 	}
 }
